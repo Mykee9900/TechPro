bc26fcd380648bf279a60db43e15989c193a3bb6
Kikes9900 <mykee5511@gmail.com>
2024-06-15 18:36:14 -0600

    Saving new changes

--- TechPro/wwwroot/js/site.js
+++ TechPro/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    var tabName = $("[id*=TabName]").val() != "" ? $("[id*=TabName]").val() : "personal";
+    new bootstrap.Tab($('#Tabs button[data-bs-target="#' + tabName + '"]')).show();
+    $("#Tabs button").click(function () {
+        $("[id*=TabName]").val($(this).attr("data-bs-target").replace("#", ""));
+    });
+});
